063efefd88d037a4969b124d4f5c35c06cedae76
Ryan Volz <ryan.volz@gmail.com>
2013-06-21 00:22:08 -0700

    Change estimators so that the index at which to estimate the peak is specified, rather than finding the maximum value.
    This way, one can find the maximum value first and decide if it is significant enough
    before proceeding with full estimation.

--- echolect/estimators/estimators.py
+++ echolect/estimators/estimators.py
@@ -21,26 +21,21 @@ import scipy.optimize
 
 from . import interpolators
 
-__all__ = ['max', 'quadratic', 'quadratic_fixedskew', 'quadratic_lstsq', 
+__all__ = ['max_idx', 'quadratic', 'quadratic_fixedskew', 'quadratic_lstsq', 
            'bicubic_hermite', 'biquintic_hermite']
 
-def max(x):
+def max_idx(x):
     max_idx = np.argmax(x)
     max_idx = np.unravel_index(max_idx, x.shape)
 
-    val = x[max_idx]
+    return max_idx
 
-    return max_idx, val
-
-def quadratic(x):
-    max_idx = np.argmax(x)
-    max_idx = np.unravel_index(max_idx, x.shape)
-
+def quadratic(x, x0_idx):
     K = np.array([-1, -1, 0, 1, 1, 0])
     L = np.array([-1, 1, 0, -1, 1, 1])
     # wrap the indices around the end because it either makes sense (Doppler)
     # or we're probably detecting noise anyway
-    z = x[(max_idx[0] + K) % x.shape[0], (max_idx[1] + L) % x.shape[1]]
+    z = x[(x0_idx[0] + K) % x.shape[0], (x0_idx[1] + L) % x.shape[1]]
     A = np.matrix([K**2, K*L, L**2, K, L, np.ones(len(K))]).T
 
     C = np.linalg.solve(A, z)
@@ -50,17 +45,14 @@ def quadratic(x):
     i1 = (2*a*e - b*d)/(b**2 - 4*a*c)
     val = f - a*i0**2 - b*i0*i1 - c*i1**2
 
-    return (max_idx[0] + i0, max_idx[1] + i1), val
+    return (x0_idx[0] + i0, x0_idx[1] + i1), val
 
-def quadratic_fixedskew(x, b=0):
-    max_idx = np.argmax(x)
-    max_idx = np.unravel_index(max_idx, x.shape)
-
+def quadratic_fixedskew(x, x0_idx, b=0):
     K = np.array([-1, 0, 0, 0, 1])
     L = np.array([0, -1, 0, 1, 0])
     # wrap the indices around the end because it either makes sense (Doppler)
     # or we're probably detecting noise anyway
-    z = x[(max_idx[0] + K) % x.shape[0], (max_idx[1] + L) % x.shape[1]]
+    z = x[(x0_idx[0] + K) % x.shape[0], (x0_idx[1] + L) % x.shape[1]]
     z0 = z - b*K*L
     A = np.matrix([K**2, L**2, K, L, np.ones(len(K))]).T
 
@@ -71,12 +63,9 @@ def quadratic_fixedskew(x, b=0):
     i1 = (2*a*e - b*d)/(b**2 - 4*a*c)
     val = f - a*i0**2 - b*i0*i1 - c*i1**2
 
-    return (max_idx[0] + i0, max_idx[1] + i1), val
+    return (x0_idx[0] + i0, x0_idx[1] + i1), val
 
-def quadratic_lstsq(x):
-    max_idx = np.argmax(x)
-    max_idx = np.unravel_index(max_idx, x.shape)
-
+def quadratic_lstsq(x, x0_idx):
     idx0 = np.array([-1, 0, 1])
     idx1 = np.array([-1, 0, 1])
     K, L = np.meshgrid(idx0, idx1)
@@ -84,49 +73,45 @@ def quadratic_lstsq(x):
     L = L.ravel()
     # wrap the indices around the end because it either makes sense (Doppler)
     # or we're probably detecting noise anyway
-    z = x[(max_idx[0] + K) % x.shape[0], (max_idx[1] + L) % x.shape[1]]
+    z = x[(x0_idx[0] + K) % x.shape[0], (x0_idx[1] + L) % x.shape[1]]
     A = np.matrix([K**2, K*L, L**2, K, L, np.ones(len(K))]).T
 
     C, _, _, _ = np.linalg.lstsq(A, z)
     a, b, c, d, e, _ = C
-    f = x[max_idx] # lstsq fit for curvature, but peak value based on max value
+    f = x[x0_idx] # lstsq fit for curvature, but peak value based on max value
 
     i0 = (2*c*d - b*e)/(b**2 - 4*a*c)
     i1 = (2*a*e - b*d)/(b**2 - 4*a*c)
     val = f - a*i0**2 - b*i0*i1 - c*i1**2
 
-    return (max_idx[0] + i0, max_idx[1] + i1), val
+    return (x0_idx[0] + i0, x0_idx[1] + i1), val
 
-def bicubic_hermite(x):
+def bicubic_hermite(x, x0_idx):
     # interpolate with bicubic Hermite spline using finite difference approximation
     # for derivatives, equivalent to cubic convolution interpolation
-    max_idx = np.argmax(x)
-    max_idx = np.unravel_index(max_idx, x.shape)
 
     # wrap the indices around the end because it either makes sense (Doppler)
     # or we're probably detecting noise anyway
     xinterp = interpolators.Bicubic(x, bc='wrap')
     f = lambda z: -xinterp.f(*z)
     fprime = lambda z: -xinterp.grad(*z)
-    x0 = np.asarray(max_idx)
+    x0 = np.asarray(x0_idx)
     xopt, fopt, d = sp.optimize.fmin_l_bfgs_b(f, x0, fprime,
                                               bounds=[(x0[0] - 1, x0[0] + 1),
                                                       (x0[1] - 1, x0[1] + 1)])
 
     return xopt, -fopt
 
-def biquintic_hermite(x):
+def biquintic_hermite(x, x0_idx):
     # interpolate with biquintic Hermite spline using finite difference approximation
     # for derivatives, equivalent to cubic convolution interpolation
-    max_idx = np.argmax(x)
-    max_idx = np.unravel_index(max_idx, x.shape)
 
     # wrap the indices around the end because it either makes sense (Doppler)
     # or we're probably detecting noise anyway
     xinterp = interpolators.Biquintic(x, bc='wrap')
     f = lambda z: -xinterp.f(*z)
     fprime = lambda z: -xinterp.grad(*z)
-    x0 = np.asarray(max_idx)
+    x0 = np.asarray(x0_idx)
     xopt, fopt, d = sp.optimize.fmin_l_bfgs_b(f, x0, fprime,
                                               bounds=[(x0[0] - 1, x0[0] + 1),
                                                       (x0[1] - 1, x0[1] + 1)])
